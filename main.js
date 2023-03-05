998951376183cbee4313ee004d517ed56977ef38
Damilola Babatunde <babatundedamilola334@gmail.com>
2023-03-05 05:36:59 -0800

    day-two

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-//  Declaring different variables of different data types
-
-let firstName = 'Damilola';    // first name of a person
-let lastName = 'Babatunde' ;     // last name of a person
-let country = 'Nigeria';     // country
-let city = 'Lagos';         // capital city
-let age = 10;             // age in years
-let isMarried = false
-
-// Declaring variables with number values
-
-const gravity = 9.81         // earth gravity  in m/s2
-const boilingPoint = 100     // water boiling point, temperature in oC
-const PI = 3.14              // geometrical constant
-
-// Variables can also be declaring in one line separated by comma
-
-let name = 'Damilola',     //name of a person
-  job = 'developer',
-  live = 'Nigeria'
 
   console.log(firstName, lastName, country, city, age, isMarried) 
 console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
