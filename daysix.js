3991e0d3bf0186337028c546f94b49e0b473b25e
Damilola Babatunde <babatundedamilola334@gmail.com>
2023-03-15 12:15:20 -0700

    Day Six

---
+++ daysix.js
@@ -0,0 +1,28 @@
+
+// for loop
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// while loop
+
+let i = 0
+while (i <= 5) {
+  console.log(i)
+  i++
+}
+
+// do while loop
+let x = 0
+    do {
+        console.log(x)
+        x ++
+    } while (x <= 10);
+
+    // for of loop
+
+    const numbers = [1, 2, 3, 4, 5]
+
+for (const num of numbers) {
+  console.log(num)
+}
