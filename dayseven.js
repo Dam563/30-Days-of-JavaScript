62f955e6193df2aa752a86f1e7272452e42141e0
Damilola Babatunde <babatundedamilola334@gmail.com>
2023-03-16 07:34:28 -0700

    Day Seven

---
+++ dayseven.js
@@ -0,0 +1,9 @@
+
+// A function is a reusable block of code or programming statements designed to perform a certain task
+
+//declaring a function without a parameter
+function functionName() {
+  // code goes here
+}
+functionName() // calling function by its name and with parentheses
+
